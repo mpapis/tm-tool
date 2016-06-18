9b9bf09e2ce7e20119937bbd549779e0f8906c63
Michal Papis <mpapis@gmail.com>
2016-06-18 23:26:00 +0200

    after migrations and set ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618205111) do
+ActiveRecord::Schema.define(version: 20160618205333) do
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "accesstoken"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "phone"
+    t.string   "urls"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "secrettoken"
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
+  create_table "user_tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.string   "installation_identifier"
+    t.string   "push_token"
+    t.boolean  "locked"
+    t.string   "form_factor"
+    t.string   "os"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "user_tokens", ["installation_identifier"], name: "index_user_tokens_on_installation_identifier"
+  add_index "user_tokens", ["user_id"], name: "index_user_tokens_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -26,9 +59,21 @@ ActiveRecord::Schema.define(version: 20160618205111) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
+    t.integer  "user_tokens_count"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count"
+  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
